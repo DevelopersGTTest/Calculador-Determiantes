72872f14789f54898dd8784713e1b84fb1e9a440
Pablo <pablojacobo1998@gmail.com>
2018-10-17 23:19:40 -0600

    Vistas Parciales

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ('express')
+const app = express()
+
+app.use( express.static( __dirname + '/public'))
+
+let port = 3000
+
+app.listen( port, ()=>{
+    console.log('servidor:  ' + port )
+})
---
+++ public/controller.js
@@ -0,0 +1,6 @@
+var val1 = document.getElementById("val1");
+
+
+ function alertt(){
+     alert('hola visistante');
+ }
